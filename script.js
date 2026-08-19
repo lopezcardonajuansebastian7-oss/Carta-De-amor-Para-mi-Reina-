fdae4adfc2157d221be30b4ef19289f8d91ae91f
lopezcardonajuansebastian7-oss <lopezcardonajuansebastian7@gmail.com>
2026-08-19 11:53:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,102 +2,17 @@ const inicio = document.getElementById("inicio");
 const carta = document.getElementById("carta");
 const corazon = document.querySelector(".corazon");
 
-let abierto = false;
+corazon.addEventListener("click", abrir);
 
+function abrir() {
 
-/* =====================================
-   ABRIR CARTA
-===================================== */
-
-function abrirCarta() {
-
-    if (abierto) {
-        return;
-    }
-
-    abierto = true;
-
-
-    /* El corazón crece */
-
-    corazon.style.animation =
-        "latido 0.5s infinite";
-
-
-    /* Desaparece la primera pantalla */
-
-    setTimeout(() => {
-
-        inicio.style.opacity = "0";
-
-        inicio.style.transform =
-            "scale(0.7)";
-
-    }, 300);
-
-
-    /* Aparece la carta */
+    inicio.style.opacity = "0";
+    inicio.style.transform = "scale(.8)";
 
     setTimeout(() => {
 
         inicio.classList.add("oculto");
-
         carta.classList.remove("oculto");
 
-        carta.style.opacity = "1";
-
-        carta.style.transform =
-            "scale(1)";
-
-    }, 1300);
-
+    }, 900);
 }
-
-
-/* =====================================
-   TOCAR CORAZÓN
-===================================== */
-
-corazon.addEventListener(
-    "click",
-    abrirCarta
-);
-
-
-/* =====================================
-   TOCAR PANTALLA
-===================================== */
-
-inicio.addEventListener(
-    "click",
-    function(event) {
-
-        if (
-            event.target === inicio ||
-            event.target.classList.contains("tocar")
-        ) {
-
-            abrirCarta();
-
-        }
-
-    }
-);
-
-
-/* =====================================
-   ENTER
-===================================== */
-
-document.addEventListener(
-    "keydown",
-    function(event) {
-
-        if (event.key === "Enter") {
-
-            abrirCarta();
-
-        }
-
-    }
-);
